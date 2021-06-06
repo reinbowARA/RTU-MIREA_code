40510df6d5a442c239239402a62bb401ee9c2f7d
reinbowARA <81460339+reinbowARA@users.noreply.github.com>
2021-06-06 21:10:28 +0300

    numbers finish
    подправил ввод и вывод

--- numbers.h
+++ numbers.h
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 // Наибольший общий делитель
 int gcd (int x, int y){
 	int g;
-	if (x<0)
+	if (x<0)// находим модуль числа х
 	   x = -x;
-    if (y<0)
+    if (y<0)// находим модуль числа у
        y = -y;
     if (x+y==0)
        printf("error");
@@ -17,28 +18,41 @@ int gcd (int x, int y){
 	return g;
 }
 //НОД n чисел
-int NodArray(int *a, int n)
-{
-   int i;
-   int g;
-   if (n < 1)
-   	  return 0;
-   g = a[0];
-   for (i = 1; i<n; ++i){
-   	g=gcd(g,a[i]);
-   	if (g == 1)
-   	return 1;
-   }
-   return g;
+int NodArray(int *a, int n){
+	a = (int*)malloc(n*sizeof(int));
+	printf ("enter kol-vo numbers: ");
+	scanf ("%d", &n);
+	for(int i = 0; i< n; i++){
+		printf("a[%d]", i);
+		scanf("%d=",&a[i]);
+	}		
+	int i;
+	int g;
+	if (n < 1)
+ 	    return 0;
+	g = a[0];
+	for (i = 1; i<n; ++i){
+		g=gcd(g,a[i]);
+		if (g == 1)
+		return 1;
+	}
+	printf ("%d",g);
+	return g;
 }
 // Наименьшее общее кратное
-int NOK(int n1, int n2) 
-{ 
+int NOK(int n1, int n2) {
+			
   return n1*n2 / gcd(n1, n2); 
 }
 // НОК n чисел
-int NOKarray(int *a, int n) 
-{ 
+int NOKarray(int *a, int n){ 
+   	a = (int*)malloc(n*sizeof(int));
+	printf ("enter kol-vo numbers: ");
+	scanf ("%d=", &n);
+	for(int i = 0; i< n; i++){
+		printf("a[%d]", i);
+		scanf("%d",&a[i]);
+	}	
    int i;
    int g;
    if (n < 1)
@@ -49,12 +63,13 @@ int NOKarray(int *a, int n)
    	if (g == 1)
    	return 1;
    }
+   printf ("%d",g);
    return g;
 }
 //Евклид 2 чисел
 int Evklidos(int a, int b){
 	int p=1, q=0, r=0, s=1, x, y;
-	    while (a && b) {
+    while (a && b) {
         if (a>=b) {
             a = a - b; 
             p = p - r; 
@@ -74,17 +89,23 @@ int Evklidos(int a, int b){
         x = r;
         y = s;
     }
-    printf("a*%d + b*%d\n",x,y);
+    printf("%d %d ",x,y);
     return x,y;
 }
 //Евклид для n чисел
 int EvklidNodArray(int *a, int n){
-
+	a = (int*)malloc(n*sizeof(int));
+	printf ("enter kol-vo numbers: ");
+	scanf ("%d", &n);
+	for(int i = 0; i< n; i++){
+		printf("a[%d]=", i);
+		scanf("%d", &a[i]);
+	}
    int i;
-   int g;
+   int g,c;
    if (n < 1)
    	  return 0;
-   printf("output\n");
+   printf("look end 2 numbers\n");
    g = a[0];
    for (i = 1; i<n; ++i){
    	  Evklidos(g,a[i]);
